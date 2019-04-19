118742b73291739f494a07b810ee26357b78efe7
Hatem Ounis <h.ounis@osram.com>
2019-04-19 02:20:34 +0200

    Add cleanup script.

---
+++ cleanup.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+set -e
+
+[[ $- == *i* ]] && tput setaf 2
+echo "Delete Camera"
+[[ $- == *i* ]] && tput sgr0
+
+curl -X DELETE \
+  "https://api.dev.olt-dev.io/v1/devices/$OLT_CAMERA_DEVICE" \
+  -H "Authorization: Bearer $OLT_TOKEN"
+
+[[ $- == *i* ]] && tput setaf 2
+echo "Delete Camera Type"
+[[ $- == *i* ]] && tput sgr0
+
+curl -X DELETE \
+  "https://api.dev.olt-dev.io/v1/device-types/$OLT_CAMERA_DEVICE_TYPE" \
+  -H "Authorization: Bearer $OLT_TOKEN"
+
+[[ $- == *i* ]] && tput setaf 2
+echo "Delete Distance"
+[[ $- == *i* ]] && tput sgr0
+
+curl -X DELETE \
+  "https://api.dev.olt-dev.io/v1/devices/$OLT_DISTANCE_DEVICE" \
+  -H "Authorization: Bearer $OLT_TOKEN"
+
+[[ $- == *i* ]] && tput setaf 2
+echo "Delete Distance Type"
+[[ $- == *i* ]] && tput sgr0
+
+curl -X DELETE \
+  "https://api.dev.olt-dev.io/v1/device-types/$OLT_DISTANCE_DEVICE_TYPE" \
+  -H "Authorization: Bearer $OLT_TOKEN"
+
+[[ $- == *i* ]] && tput setaf 2
+echo "Delete Presence"
+[[ $- == *i* ]] && tput sgr0
+
+curl -X DELETE \
+  "https://api.dev.olt-dev.io/v1/devices/$OLT_PRESENCE_DEVICE" \
+  -H "Authorization: Bearer $OLT_TOKEN"
+
+[[ $- == *i* ]] && tput setaf 2
+echo "Delete Presence Type"
+[[ $- == *i* ]] && tput sgr0
+
+curl -X DELETE \
+  "https://api.dev.olt-dev.io/v1/device-types/$OLT_PRESENCE_DEVICE_TYPE" \
+  -H "Authorization: Bearer $OLT_TOKEN"
+
+[[ $- == *i* ]] && tput setaf 2
+echo "Delete Rgb"
+[[ $- == *i* ]] && tput sgr0
+
+curl -X DELETE \
+  "https://api.dev.olt-dev.io/v1/devices/$OLT_RGB_DEVICE" \
+  -H "Authorization: Bearer $OLT_TOKEN"
+
+[[ $- == *i* ]] && tput setaf 2
+echo "Delete Rgb Type"
+[[ $- == *i* ]] && tput sgr0
+
+curl -X DELETE \
+  "https://api.dev.olt-dev.io/v1/device-types/$OLT_RGB_DEVICE_TYPE" \
+  -H "Authorization: Bearer $OLT_TOKEN"
+
+[[ $- == *i* ]] && tput setaf 2
+echo "Delete Screen"
+[[ $- == *i* ]] && tput sgr0
+
+curl -X DELETE \
+  "https://api.dev.olt-dev.io/v1/devices/$OLT_SCREEN_DEVICE" \
+  -H "Authorization: Bearer $OLT_TOKEN"
+
+[[ $- == *i* ]] && tput setaf 2
+echo "Delete Screen Type"
+[[ $- == *i* ]] && tput sgr0
+
+curl -X DELETE \
+  "https://api.dev.olt-dev.io/v1/device-types/$OLT_SCREEN_DEVICE_TYPE" \
+  -H "Authorization: Bearer $OLT_TOKEN"
+
+[[ $- == *i* ]] && tput setaf 2
+echo "Delete Raspberry"
+[[ $- == *i* ]] && tput sgr0
+
+curl -X DELETE \
+  "https://api.dev.olt-dev.io/v1/devices/$OLT_RASPBERRY_DEVICE" \
+  -H "Authorization: Bearer $OLT_TOKEN"
+
+[[ $- == *i* ]] && tput setaf 2
+echo "Delete Raspberry Type"
+[[ $- == *i* ]] && tput sgr0
+
+curl -X DELETE \
+  "https://api.dev.olt-dev.io/v1/device-types/$OLT_RASPBERRY_DEVICE_TYPE" \
+  -H "Authorization: Bearer $OLT_TOKEN"
