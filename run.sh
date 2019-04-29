9a9a6b04dd5eded5a75a452bbf229529ce552714
Hatem Ounis <h.ounis@osram.com>
2019-04-29 13:23:54 +0200

    Set env variables in docker.

--- run.sh
+++ run.sh
@@ -2,34 +2,34 @@
 
 export OLT_TOKEN="${OLT_TOKEN}"
 export OLT_TENANT="${OLT_TENANT}"
-./raspbiansetup.sh
+bash /home/pi/raspbiansetup.sh
 export OLT_RASPBERRY_DEVICE=`cat out/raspberry.txt`
 export OLT_RASPBERRY_DEVICE_TYPE=`cat out/raspberry_type.txt`
 
-./install_camera.sh
+bash /home/pi/install_camera.sh
 export OLT_CAMERA_DEVICE=`cat out/camera.txt`
 export OLT_CAMERA_DEVICE_TYPE=`cat out/camera_type.txt`
 
-./install_screen.sh
+bash /home/pi/install_screen.sh
 export OLT_SCREEN_DEVICE=`cat out/screen.txt`
 export OLT_SCREEN_DEVICE_TYPE=`cat out/screen_type.txt`
 
-./install_distance.sh
+bash /home/pi/install_distance.sh
 export OLT_DISTANCE_DEVICE=`cat out/distance.txt`
 export OLT_DISTANCE_DEVICE_TYPE=`cat out/distance_type.txt`
 
-./install_presence.sh
+bash /home/pi/install_presence.sh
 export OLT_PRESENCE_DEVICE=`cat out/presence.txt`
 export OLT_PRESENCE_DEVICE_TYPE=`cat out/presence_type.txt`
 
-./install_rgb.sh
+bash /home/pi/install_rgb.sh
 export OLT_RGB_DEVICE=`cat out/rgb.txt`
 export OLT_RGB_DEVICE_TYPE=`cat out/rgb_type.txt`
 
-./install_hue.sh
+bash /home/pi/install_hue.sh
 export OLT_HUE_DEVICE=`cat out/hue.txt`
 export OLT_HUE_DEVICE_TYPE=`cat out/hue_type.txt`
 
-./install_link.sh
+bash /home/pi/install_link.sh
 
-./cleanup.sh
+bash /home/pi/cleanup.sh
